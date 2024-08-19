398f8728f8e183d007a697a43de7a619dd1fa89a
erick <esierra@dslrenovables.com>
2024-08-19 15:48:21 -0400

    mark 0.1

--- T1/palin.c
+++ T1/palin.c
@@ -4,5 +4,28 @@
 #include "palin.h"
 
 int palindrome(uint32_t x) {
-  // Complete
+  printf("entrada: %x\n", x);
+
+   // int i = 0;
+   // while (i <= 28) {
+   //   printf("LEFT i = %d: %x\n",i, x<<i);
+   //   i = i + 4;
+   // }
+  //
+  // int j = 0;
+  // while (j <= 28) {
+  //   printf("RIGHT j = %d: %x\n",i, x>>j);
+  //   j = j + 4;
+  // }
+
+  // el ultimo se obtiene con x<<28 (28 = 7 * 4)
+  // el primero se obtiene con x>>28 (28 = 7 * 4)
+
+  uint32_t first = x >> 28;
+  uint32_t last = x << 28;
+
+  printf("First: %x\n", first);
+  printf("Last: %x\n", last >> 28);
+
+  return 0;
 }
---
+++ T1/test/test.c
@@ -0,0 +1,10 @@
+//
+// Created by erick on 8/19/24.
+//
+#include <stdio.h>
+#include "../palin.c"
+
+void main() {
+  int res = palindrome(0xA234567A);
+  printf("respuesta: %d\n", res);
+}
