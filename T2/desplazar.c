d139a607e1ebbfa3824c2f72774a60ace0a37499
emsb-1991 <erick.sierra@ing.uchile.cl>
2024-09-01 21:04:43 -0400

    agrega T2

---
+++ T2/desplazar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "desplazar.h"
+
+void desplazar(char *str, int from, int nbytes, int by) {
+  // complete
+}
+
+char *desplazamiento(char *str, int from, int nbytes, int by) {
+  // complete
+}
